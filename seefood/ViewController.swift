99ba06453183395a030071de83b1ac1766e9ea32
Barış <barisgungor@Barss-Mac.local>
2019-04-06 07:40:00 -0700

    Initial Commit

---
+++ seefood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seefood
+//
+//  Created by Barış Güngör on 6.04.2019.
+//  Copyright © 2019 Baris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
